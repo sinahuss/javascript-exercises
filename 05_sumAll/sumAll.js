a11f341daaa9fbcec55223bc69e29bd8277c005d
sinahuss <hussaini.sina@gmail.com>
2022-08-19 23:09:03 -0700

    Solve exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(typeof start != 'number' || typeof end != 'number') return 'ERROR';
+    if(start < 0 || end < 0) return 'ERROR';
+    if (start > end) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+    let ans = 0;
+    for(i=start;i<=end;i++) {
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
