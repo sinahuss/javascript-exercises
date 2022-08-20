4278248ded69641f2918205c4cba1a54f9f3357c
sinahuss <hussaini.sina@gmail.com>
2022-08-19 20:58:47 -0700

    Complete exercise #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var ans = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for(i=0;i<num;i++) {
+        ans += string;
+    }
+    return ans;
 };
 
 // Do not edit below this line
