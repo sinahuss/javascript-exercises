ec3ef50d05174cb25c799aa11c9d6547aaee0a11
sinahuss <hussaini.sina@gmail.com>
2022-08-19 21:05:00 -0700

    Finish exercise #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var ans = '';
+    for(i=0;i<string.length;i++) {
+        ans += string[string.length-1-i];
+    }
+    return ans;
 };
 
 // Do not edit below this line
